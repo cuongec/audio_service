28b22ad0767297e01900b962fc3bdf169680d1ea
cuongec <166884982+cuongec@users.noreply.github.com>
2024-09-27 16:57:06 +0700

    Fix notification does not remove when stop audio

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
@@ -352,6 +352,7 @@ public class AudioService extends MediaBrowserServiceCompat {
     }
 
     public void stop() {
+        exitPlayingState();
         deactivateMediaSession();
         stopSelf();
     }
